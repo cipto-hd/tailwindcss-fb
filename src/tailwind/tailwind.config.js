d14bc25c8ba8d72a3dd0b5bf4deea994ce6d7acd
Cipto Hadi <ciptohadi79@gmail.com>
2021-04-27 12:36:46 +0700

    add 3xl screen breakpoint 4k-2560px

--- src/tailwind/tailwind.config.js
+++ src/tailwind/tailwind.config.js
@@ -1,22 +1,25 @@
 module.exports = {
-  purge: ['../*.html'],
-  darkMode: 'class', // or 'media' or 'class'
+  purge: ["../*.html"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        'dark-main': '#18191a',
-        'dark-second': '#242526',
-        'dark-third': '#3a3b3c',
-        'dark-text': '#b8bbbf',
-      }
+        "dark-main": "#18191a",
+        "dark-second": "#242526",
+        "dark-third": "#3a3b3c",
+        "dark-text": "#b8bbbf",
+      },
+      screens: {
+        "3xl": "2560px",
+      },
     },
   },
   variants: {
     extend: {
-      display: ['group-hover'],
-      transform : ['group-hover'],
-      scale: ['group-hover'],
+      display: ["group-hover"],
+      transform: ["group-hover"],
+      scale: ["group-hover"],
     },
   },
   plugins: [],
-}
+};
